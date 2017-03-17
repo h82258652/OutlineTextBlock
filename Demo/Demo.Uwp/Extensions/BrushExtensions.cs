f70063bf346aa1ad1782ba5ae21ce2a4772b3b68
h82258652 <h82258652@hotmail.com>
2017-03-17 16:27:32 +0800

    update

---
+++ Demo/Demo.Uwp/Extensions/BrushExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using Windows.UI.Xaml.Media;
+using Microsoft.Graphics.Canvas;
+using Microsoft.Graphics.Canvas.Brushes;
+
+namespace Demo.Uwp.Extensions
+{
+    public static class BrushExtensions
+    {
+        public static ICanvasBrush ToCanvasBrush(this Brush brush, ICanvasResourceCreator resourceCreator)
+        {
+            switch (brush)
+            {
+                case SolidColorBrush solidColorBrush:
+                    return new CanvasSolidColorBrush(resourceCreator, solidColorBrush.Color);
+
+                case LinearGradientBrush linearGradientBrush:
+
+                    return new CanvasLinearGradientBrush(resourceCreator, (from GradientStop gradientStop in linearGradientBrush.GradientStops
+                                                                           select new CanvasGradientStop()
+                                                                           {
+                                                                               Color = gradientStop.Color,
+                                                                               Position = (float)gradientStop.Offset
+                                                                           }).ToArray());
+
+                default:
+                    throw new NotSupportedException();
+            }
+        }
+    }
+}
--- Demo/Demo.Uwp/OutlineTextBlock2.cs
+++ Demo/Demo.Uwp/OutlineTextBlock2.cs
@@ -1,23 +1,59 @@
-﻿using System;
-using Windows.UI;
-using Windows.UI.Composition;
-using Windows.UI.Xaml;
+﻿using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Microsoft.Graphics.Canvas.Brushes;
-using Microsoft.Graphics.Canvas.Geometry;
 using Microsoft.Graphics.Canvas.Text;
 using Microsoft.Graphics.Canvas.UI.Xaml;
+using System;
+using Windows.Foundation;
+using Windows.UI;
+using Windows.UI.Composition;
+using Windows.UI.Xaml.Hosting;
+using Demo.Uwp.Extensions;
+using Microsoft.Graphics.Canvas;
+using Microsoft.Graphics.Canvas.Brushes;
+using Microsoft.Graphics.Canvas.Geometry;
 
 namespace Demo.Uwp
 {
     public class OutlineTextBlock2 : Control
     {
+        public static readonly DependencyProperty StrokeThicknessProperty = DependencyProperty.Register(nameof(StrokeThickness), typeof(double), typeof(OutlineTextBlock2), new PropertyMetadata(default(double)));
+
+        public static readonly DependencyProperty TextProperty = DependencyProperty.Register(nameof(Text), typeof(string), typeof(OutlineTextBlock2), new PropertyMetadata(default(string), OnTextChanged));
+
         private CanvasControl _canvas;
+
+        private CanvasTextLayout _textLayout;
 
         public OutlineTextBlock2()
         {
             DefaultStyleKey = typeof(OutlineTextBlock2);
             Unloaded += OutlineTextBlock2_Unloaded;
+
+            StrokeThickness = 1;
+        }
+
+        public double StrokeThickness
+        {
+            get
+            {
+                return (double)GetValue(StrokeThicknessProperty);
+            }
+            set
+            {
+                SetValue(StrokeThicknessProperty, value);
+            }
+        }
+
+        public string Text
+        {
+            get
+            {
+                return (string)GetValue(TextProperty);
+            }
+            set
+            {
+                SetValue(TextProperty, value);
+            }
         }
 
         protected override void OnApplyTemplate()
@@ -28,33 +64,21 @@ namespace Demo.Uwp
             _canvas.Draw += Canvas_Draw;
         }
 
-        private CanvasTextLayout _textLayout;
-
-        public string Text
+        private static void OnTextChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
-            get
-            {
-                throw new NotImplementedException();
-            }
-            set
-            {
-                throw new NotImplementedException();
-            }
+            var obj = (OutlineTextBlock2)d;
+
+            obj._canvas?.Invalidate();
         }
 
-        public double StrokeThickness
+        public TextWrapping TextWrapping
         {
-            get
-            {
-            }
-            set
-            { }
-        }
+            get;
+            set;
+        } = TextWrapping.NoWrap;
 
         private void Canvas_Draw(CanvasControl sender, CanvasDrawEventArgs args)
         {
-            new Compositor().
-
             var textFormat = new CanvasTextFormat()
             {
                 FontSize = (float)FontSize,
@@ -63,14 +87,27 @@ namespace Demo.Uwp
                 FontWeight = FontWeight,
                 FontStyle = FontStyle
             };
+            switch (TextWrapping)
+            {
+                case TextWrapping.NoWrap:
+                    textFormat.WordWrapping = CanvasWordWrapping.NoWrap;
+                    break;
+
+                case TextWrapping.Wrap:
+                    textFormat.WordWrapping = CanvasWordWrapping.Wrap;
+                    break;
+
+                case TextWrapping.WrapWholeWords:
+                    textFormat.WordWrapping = CanvasWordWrapping.WholeWord;
+                    break;
+
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(TextWrapping));
+            }
 
             _textLayout = new CanvasTextLayout(sender, Text, textFormat, (float)sender.Size.Width, (float)sender.Size.Height);
 
-            //var canvasSolidColorBrush = new CanvasSolidColorBrush(sender, Color.FromArgb(1, 1, 1, 1));
-
-            //args.DrawingSession.DrawGeometry(CanvasGeometry.CreateText(_textLayout), Colors.Aqua , StrokeThickness , new CanvasStrokeStyle()
-            //{
-            //});
+            args.DrawingSession.DrawGeometry(CanvasGeometry.CreateText(_textLayout), Foreground.ToCanvasBrush(sender), (float)StrokeThickness);
         }
 
         private void OutlineTextBlock2_Unloaded(object sender, RoutedEventArgs e)
